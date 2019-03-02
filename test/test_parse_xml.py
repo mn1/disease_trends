ec3215c378a3f08d5123988a894b0a07f6d80b31
Michael Nuhn <mnuhn@ebi.ac.uk>
2019-03-02 09:58:54 +0000

    moved data files

--- test/test_parse_xml.py
+++ test/test_parse_xml.py
@@ -6,26 +6,28 @@ import unittest
 import os
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
+data_files_dir = THIS_DIR + '/data'
+
 class TestStringMethods(unittest.TestCase):
 
     def test_xml_parse_ok(self):
         
-        count = parse_xml(THIS_DIR + '/' + 'test.xml')
+        count = parse_xml(data_files_dir + '/' + 'test.xml')
         self.assertEqual(count, "37901")
 
-        count = parse_xml(THIS_DIR + '/' + 'test_2011.xml')
+        count = parse_xml(data_files_dir + '/' + 'test_2011.xml')
         self.assertEqual(count, "176207")
 
-        count = parse_xml(THIS_DIR + '/' + 'test_2012.xml')
+        count = parse_xml(data_files_dir + '/' + 'test_2012.xml')
         self.assertEqual(count, "188997")
         
-        count = parse_xml(THIS_DIR + '/' + 'test_2013.xml')
+        count = parse_xml(data_files_dir + '/' + 'test_2013.xml')
         self.assertEqual(count, "196630")
         
-        count = parse_xml(THIS_DIR + '/' + 'test_2014.xml')
+        count = parse_xml(data_files_dir + '/' + 'test_2014.xml')
         self.assertEqual(count, "210073")
         
-        count = parse_xml(THIS_DIR + '/' + 'test_2015.xml')
+        count = parse_xml(data_files_dir + '/' + 'test_2015.xml')
         self.assertEqual(count, "217904")
 
 if __name__ == '__main__':
