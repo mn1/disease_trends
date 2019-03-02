dc1ca3f7d30f54e329190739a5aa699f27f9b77a
Michael Nuhn <mnuhn@ebi.ac.uk>
2019-03-02 19:25:45 +0000

    various

--- commands.bash
+++ commands.bash
@@ -40,8 +40,32 @@ python mysite/manage.py test polls
 
 # Starting the disease trends app
 
-django-admin startproject disease_trends
-#python manage.py startapp disease_trends
+django-admin startproject modules/disease_trends
+
+python disease_trends/manage.py startapp get_input
+python modules/disease_trends/manage.py runserver 0:8000
+
+
+
+
+
+sudo yum -y install python36u
+
+
+
+ssh -i /home/mnuhn/linguamatics/aws_disease_trends.pem ec2-user@35.178.239.253
+
+
+35.178.239.253:8000
+
+
+amazon-linux-extras -y install python36u
+
+
+
+
+
+
 
 
 
