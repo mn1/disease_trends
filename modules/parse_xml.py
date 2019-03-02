48c39417143c34f817446f232fbd4312b77b57b5
Michael Nuhn <mnuhn@ebi.ac.uk>
2019-03-02 19:24:26 +0000

    added method that can parse the data from an xml string

--- modules/parse_xml.py
+++ modules/parse_xml.py
@@ -5,7 +5,14 @@ import xml.etree.ElementTree
 def parse_xml(file_name):
     
     e = xml.etree.ElementTree.parse(file_name).getroot()
+    return _get_count_from_element_tree(e)
+
+def parse_xml_string(xml_string):
     
+    e = xml.etree.ElementTree.fromstring(xml_string)
+    return _get_count_from_element_tree(e)
+    
+def _get_count_from_element_tree(e):
     expected_root_element = "eSearchResult"
     
     if (e.tag != expected_root_element):
@@ -18,4 +25,3 @@ def parse_xml(file_name):
     if (count_element is None):
         raise Exception("XML parse error: Could not find field with count: %s" % (count_field_name));
     return count_element.text
-
