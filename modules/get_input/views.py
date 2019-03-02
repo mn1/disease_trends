e41f55c3112f078e8bb313338a134d14378309f8
Michael Nuhn <mnuhn@ebi.ac.uk>
2019-03-02 19:58:32 +0000

    updated the view

--- modules/get_input/views.py
+++ modules/get_input/views.py
@@ -4,6 +4,10 @@ from django.http import HttpResponse
 from django.template import loader
 
 from construct_urls import build_esearch_url_year_range
+from fetch_from_url \
+    import \
+        fetch_term_count_for_year, \
+        fetch_term_count_for_year_range
 
 # Create your views here.
 def index(request):
@@ -26,12 +30,18 @@ def response(request):
         from_year = start_year,
         to_year   = end_year
     )
+    
+    counts = fetch_term_count_for_year_range(name, start_year, end_year)
+    #counts = {2002: 99330, 2003: 105142, 2004: 113432, 2005: 123584, 2006: 129036, 2007: 136862, 2008: 145089, 2009: 151514, 2010: 167665}
+    #counts = {2010: 167665, 2011: 176218, 2012: 189013}
 
+    
     context = {
         'name'       : name,
         'start_year' : start_year,
         'end_year'   : end_year,
         'urls'       : urls,
+        'counts'     : counts
     }
     
     response = HttpResponse(
